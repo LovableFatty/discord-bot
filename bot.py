ce36f442e03bbc046224f1793e147f2716d02a8c
Noah <naouki.k.v@gmail.com>
2024-12-25 02:09:20 -0800

    inital bot

---
+++ bot.py
@@ -0,0 +1,38 @@
+import os
+import discord
+import random
+
+from dotenv import load_dotenv
+from discord.ext import commands
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.message_content = True  # Enable the message content intent if you need to read messages
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    for guild in bot.guilds:
+        print(f'Guild Name: {guild.name}')
+
+bot.run(TOKEN)
